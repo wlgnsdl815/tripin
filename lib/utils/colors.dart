596c17875b585a0e6427c94195f900d724507291
2Juzzang <skyy630@naver.com>
2023-09-08 16:13:20 +0900

    Feat: add utils/colors #14

---
+++ lib/utils/colors.dart
@@ -0,0 +1,19 @@
+// 🐦 Flutter imports:
+import 'package:flutter/painting.dart';
+
+abstract class PlatformColors {
+  static Color primary = const Color(0xff4D80FF);
+  static Color primaryLight = const Color(0xff6FA0FE);
+  static Color secondary = const Color(0xffFFD951);
+  static Color negative = const Color(0xffFF7272);
+
+  static Color title = const Color(0xff323232);
+  static Color subtitle = const Color(0xff585858);
+  static Color subtitle2 = const Color(0xff7B7B7B);
+  static Color subtitle3 = const Color(0xff9A9A9A);
+  static Color subtitle4 = const Color(0xffA6A6A6);
+  static Color subtitle5 = const Color(0xffB3B3B3);
+  static Color subtitle6 = const Color(0xffCDCDCD);
+  static Color subtitle7 = const Color(0xffEAEAEA);
+  static Color subtitle8 = const Color(0xffF9F9F9);
+}
