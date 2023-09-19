3aff2debe031349b5dee41e53349efece47c5370
gong <ddddy0114@gmail.com>
2023-09-19 18:01:15 +0900

    Feat. 유저 상세 진행중인 여행 표시

--- lib/controllers/profile_detail_controller.dart
+++ lib/controllers/profile_detail_controller.dart
@@ -2,6 +2,7 @@
 import 'dart:developer';
 
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 import 'package:tripin/service/db_service.dart';
 
 import '../model/chat_room_model.dart';
@@ -13,12 +14,14 @@ class ProfileDetailController extends GetxController {
   RxList<ChatRoom?> ongoingTrips = <ChatRoom?>[].obs;       // 진행중인 여행 리스트
   RxList<ChatRoom?> upcomingTrips = <ChatRoom?>[].obs;      // 예정된 여행 리스트
   RxList<ChatRoom?> completedTrips = <ChatRoom?>[].obs;     // 완료된 여행 리스트
+  RxBool isLoading = false.obs;   // 로딩중 상태
 
+  RxInt filterIdx = 0.obs;        // 필터링 선택값
   List<String> filterOptionList = ['예정', '종료'];   // 필터링 옵션 리스트
-  RxInt filterIdx = 0.obs;  // 필터링 선택값
 
   // 참여중인 채팅방 리스트 가져오기
   Future<void> readJoinedChatRoom() async {
+    isLoading(true);
     if (user.joinedTrip != null) {
       List<ChatRoom?> chatRoomList = await Future.wait(
         user.joinedTrip!.map((roomId) async {
@@ -31,11 +34,9 @@ class ProfileDetailController extends GetxController {
       DateTime now = DateTime.now();
       for (var room in chatRoomList) {
         if (room != null && room.startDate != null && room.endDate != null) {
-          DateTime startDate = DateTime.fromMillisecondsSinceEpoch(room.startDate!);
-          DateTime endDate = DateTime.fromMillisecondsSinceEpoch(room.endDate!);
-          if (endDate.isBefore(now)) {
+          if (room.endDate!.isBefore(now)) {
             completedTrips.add(room);
-          } else if (startDate.isBefore(now) && endDate.isAfter(now)) {
+          } else if (room.startDate!.isBefore(now) && room.endDate!.isAfter(now)) {
             ongoingTrips.add(room);
           } else {
             upcomingTrips.add(room);
@@ -45,9 +46,19 @@ class ProfileDetailController extends GetxController {
         }
       }
     }
+    isLoading(false);
   }
 
-  // 필터링
+  // 날짜 형식 변환
+  void formatDateTime(int timestamp) {
+    DateTime tripStartDate = DateTime.fromMillisecondsSinceEpoch(timestamp);
+    String formattedDateTime = DateFormat('yyyy.MM.dd').format(tripStartDate);
+  }
+
+  String getDayOfWeek(DateTime date) {
+    List<String> daysOfWeek = ['월', '화', '수', '목', '금', '토', '일'];
+    return daysOfWeek[date.weekday - 1];
+  }
 
   @override
   void onInit() async {
---
+++ lib/view/widget/profile_trip_tile.dart
@@ -0,0 +1,22 @@
+
+import 'package:flutter/material.dart';
+import 'package:tripin/model/chat_room_model.dart';
+import 'package:tripin/utils/colors.dart';
+
+class ProfileTripTile extends StatelessWidget {
+  const ProfileTripTile({super.key, required this.trip});
+
+  final ChatRoom trip;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      decoration: BoxDecoration(
+        color: PlatformColors.subtitle8
+      ),
+      child: Text('${trip.roomTitle ?? 'test'}'),
+    );
+  }
+}
+
