502ebc253363664d5f6891976b0d53fd68a56f39
wlgnsdl815 <wlgnsdl815@gmail.com>
2023-09-29 23:09:16 +0900

    Wip: 캘린더 작업 중

---
+++ lib/view/screens/calendar_screen_jihoon.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CalendarScreen2 extends StatelessWidget {
+  const CalendarScreen2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
